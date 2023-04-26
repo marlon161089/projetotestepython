8d66db96450a933421aae174e56cc90480619ed7
marlon161089 <130089486+marlon161089@users.noreply.github.com>
2023-04-26 12:45:56 -0300

    Add files via upload

---
+++ projetodeteste.py
@@ -0,0 +1,15 @@
+for tech_skill in "Python", "Liderença Tecnica", "PMP", "Jana", "Alige Management":
+    if (tech_skill == "java" or tech_skill == "Python"):
+        print(tech_skill, "Developer")
+        continue
+    
+    if (tech_skill == "Alige Management"):
+        break
+    
+    if (tech_skill == "Liderença Tecnica"):
+        print(tech_skill, "tech lead")
+    
+    if (tech_skill == "PMP"):
+        print(tech_skill, "Project Maneger")
+
+print("Pode continuar")
